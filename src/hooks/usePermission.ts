a4b4bab369420dc8d8dabc53b17f3d224ae34599
pjflanagan <pjflanagan1@gmail.com>
2021-09-22 00:01:57 -0400

    safari navigator

--- src/hooks/usePermission.ts
+++ src/hooks/usePermission.ts
@@ -4,7 +4,7 @@ export const usePermission = (permission: PermissionName): string => {
   const [state, setState] = useState<string>('');
 
   const handlePermission = () => {
-    navigator.permissions.query({ name: permission }).then(result => {
+    navigator.permissions?.query({ name: permission }).then(result => {
       setState(result.state);
       result.onchange = () => {
         setState(result.state);
@@ -13,7 +13,11 @@ export const usePermission = (permission: PermissionName): string => {
   }
 
   useEffect(() => {
-    handlePermission();
+    if (navigator.permissions) {
+      handlePermission();
+    } else {
+      setState('prompt');
+    }
   }, []);
 
   return state;
