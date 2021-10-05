6aaad42a1409a812f515872fe21b857cbec0ff02
pjflanagan <pjflanagan1@gmail.com>
2021-10-05 00:15:50 -0400

    slogan and share cards

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -1,40 +1,42 @@
+
 import React from 'react';
 import { Helmet } from "react-helmet";
+
+const TITLE = 'The Drunk Weather Channel';
+const DESCRIPTION = 'The only trusted name in drunk weather';
+const KEYWORDS = `drunk, weather, temperature, drinks, channel`;
+const ROOT = "https://drunkweatherchannel.flanny.app";
+
+const IMG = `${ROOT}/img/social/social.png`;
+const IMG_TW = `${ROOT}/img/social/social-tw.png`;
+
+const AUTHOR = "Peter James Flanagan";
+const HANDLE = "@peterjflan";
 
 export const HeadComponent = () => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
-      <meta content="text/html; charset=utf-8" httpEquiv="Content-Type" />
-      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 
-      <meta name="description" content="The Drunk Weather Channel" />
-      <meta name="keywords" content="drunk, weather, temperature, drinks, channel" />
+      <title>{TITLE}</title>
+      <meta name="description" content={DESCRIPTION} />
+      <meta name="author" content={AUTHOR} />
+      <meta name="keywords" content={KEYWORDS} />
 
-      {/* <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
-
-      {/* <!-- ios -->
-      <link rel="apple-touch-icon" href="%PUBLIC_URL%/img/logo192.png" />
-      <!-- <link rel="apple-touch-icon" sizes="57x57" href="//img/icon-57.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="//img/icon-72.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="//img/icon-114.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="//icon-144.png" /> --> */}
-
-      {/* <!-- facebook -->
-      <meta property="og:url" content="http://app.bamboolearn.com">
+      <meta property="og:title" content={TITLE} />
+      <meta property="og:description" content={DESCRIPTION} />
+      <meta property="og:image" content={IMG} />
+      <meta property="og:url" content={ROOT} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Bamboo Learn" />
-      <meta property="og:description" content="Learn Mandarin with this helpful Chrome Extension!" />
-      <meta property="og:image" content="%PUBLIC_URL%/img/social-media-fb.png" /> */}
+      <meta property="og:image:secure_url" content={IMG} />
 
-      {/* <meta name="twitter:site"           content="@" />
-      <meta name="twitter:creator" 		content="@"
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:title" content="Bamboo Learn" />
-      <meta name="twitter:description" content="Learn Mandarin with this helpful Chrome Extension!" />
-      <meta name="twitter:image:src" content="%PUBLIC_URL%/img/social-media-tw.png" /> */}
-
-      <title>{'The Drunk Weather Channel'} </title>
+      <meta name="twitter:title" content={TITLE} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={HANDLE} />
+      <meta name="twitter:creator" content={HANDLE} />
+      <meta name="twitter:image" content={IMG_TW} />
     </Helmet>
   );
 }
